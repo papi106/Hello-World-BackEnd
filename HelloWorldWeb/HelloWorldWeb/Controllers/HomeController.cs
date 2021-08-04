ddcda59b7e96e17d0d131bdf14e1792d1cef777d
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-04 20:54:58 +0300

    Created TeamInfo.cs and edited the index and HomeController to show easier the TeamMembers

--- HelloWorldWeb/HelloWorldWeb/Controllers/HomeController.cs
+++ HelloWorldWeb/HelloWorldWeb/Controllers/HomeController.cs
@@ -12,15 +12,25 @@ namespace HelloWorldWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TeamInfo teamInfo;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            teamInfo = new TeamInfo();
+            teamInfo.Name = "Team 1";
+            teamInfo.TeamMembers = new List<string>();
+            teamInfo.TeamMembers.Add("Emma");
+            teamInfo.TeamMembers.Add("Fineas");
+            teamInfo.TeamMembers.Add("Radu P");
+            teamInfo.TeamMembers.Add("Tudor");
+            teamInfo.TeamMembers.Add("Patrick");
+
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(teamInfo);
         }
 
         public IActionResult Privacy()
