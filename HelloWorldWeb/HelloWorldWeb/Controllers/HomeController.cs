59a4f54208c863e5e0fc665ecdaf659fc94b21c3
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-04 21:31:48 +0300

    Added StyleCop.json and modified the HomeController and HelloWorldWeb

--- HelloWorldWeb/HelloWorldWeb/Controllers/HomeController.cs
+++ HelloWorldWeb/HelloWorldWeb/Controllers/HomeController.cs
@@ -25,6 +25,19 @@ namespace HelloWorldWeb.Controllers
 
         }
 
+        [HttpGet]
+        public void AddTeamMember(string name)
+        {
+            teamInfo.TeamMembers.Add(name);
+
+        }
+
+        [HttpGet]
+        public int GetCount()
+        {
+            return teamInfo.TeamMembers.Count;
+        }
+
         public IActionResult Index()
         {
             return View(teamInfo);
