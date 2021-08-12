e598ac5c6929e281518754dc950594627d94bb26
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-12 11:26:07 +0300

    Used RestClient object to return the response as content by GET method.

--- HelloWorldWeb/HelloWorldWeb/Controllers/WeatherController.cs
+++ HelloWorldWeb/HelloWorldWeb/Controllers/WeatherController.cs
@@ -1,5 +1,6 @@
 ﻿using HelloWorldWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,30 @@ namespace HelloWorldWeb.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
+        private readonly string latitude = "46.7700";
+        private readonly string longitude = "23.5800";
+        private readonly string apiKey = "87c518ece5346b1e8f0e944352222508";
+
         // GET: api/<WeatherController>
         [HttpGet]
         public IEnumerable<DailyWeatherRecord> Get()
         {
+            //lat 46.7700 lon 23.5800
+            //var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat=46.7700&lon=23.5800&exclude=hourly,minutely&appid=87c518ece5346b1e8f0e944352222508");
+
+            var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}");
+            client.Timeout = -1;            //no timeout at all
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return ConvertResponseToWeatherForecastList(response.Content);            
+            
+        }
+
+        private IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherForecastList(string content)
+        {
             return new DailyWeatherRecord[] {
-                new DailyWeatherRecord(new DateTime(2021,8,12),22,WeatherType.Mild)
+                    new DailyWeatherRecord(new DateTime(2021,8,12),22.0f,WeatherType.Mild),
+                    new DailyWeatherRecord(new DateTime(2021,8,13),27.0f,WeatherType.Mild),
             };
         }
 
