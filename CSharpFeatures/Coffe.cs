e8d9430e8de7ef3ac92afd0a6ce7690a09d5ba96
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-19 10:58:06 +0300

    Added coffe type for customer input

---
+++ CSharpFeatures/Coffe.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpFeatures
+{
+    internal class Coffe
+    {
+        public Coffe()
+        {
+
+        }
+        public Coffe(string type)
+        {
+            CoffeType = type;
+        }
+        public string CoffeType { get; }
+
+        public override string ToString()
+        {
+            return CoffeType;
+        }
+
+    }
+}
--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -10,18 +10,52 @@ namespace CSharpFeatures
         static void Main(string[] args)
         {
 
-            TeamMember teamMember = new TeamMember() { Name = "Member 1" };
+/*            TeamMember teamMember = new TeamMember() { Name = "Member 1" };
             string jsonString = JsonSerializer.Serialize(teamMember);
+
             Console.WriteLine(jsonString);
 
             File.WriteAllText("file.json", jsonString);
-
             var readText = File.ReadAllTextAsync("file.json");
             readText.Wait();
             var expectedOutput = readText.Result;
-
             var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(expectedOutput);
-            Console.WriteLine(teamMemberDeserialized);
+
+            Console.WriteLine(teamMemberDeserialized);*/
+
+            Console.Write("What would you like? ");
+            var customerInput = Console.ReadLine();
+
+            Func<string, string, string, string, Coffe> recipe = (customerInput == "FlatWhite") ? FlatWhite : Espresso;
+            Coffe coffe = MakeCoffe("grain", "milk", "water", "sugar", recipe);
+            Console.WriteLine($"Here is your coffe: {coffe} .");
+        }
+
+        static Coffe MakeCoffe(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffe> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing coffe.");
+                var coffe = recipe(grains, milk, water, sugar);
+                return coffe;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+        }
+
+        static Coffe Espresso(string grains, string milk, string water, string sugar)
+        {
+            return new Coffe("Espresso");
+        }
+        static Coffe FlatWhite(string grains, string milk, string water, string sugar)
+        {
+            return new Coffe("Flat White");
         }
     }
 }
