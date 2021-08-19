439dff204145f9809464e2b35ef2db85d2b60ede
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-19 09:46:19 +0300

    added a new console in order to learn about serialization

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            TeamMember teamMember = new TeamMember() { Name = "Member 1" };
+
+            string jsonString = JsonSerializer.Serialize(teamMember);
+
+            Console.WriteLine(jsonString);
+
+            File.WriteAllText("file.txt", jsonString);
+
+            var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(jsonString);
+        }
+    }
+}
---
+++ CSharpFeatures/TeamMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFeatures
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
