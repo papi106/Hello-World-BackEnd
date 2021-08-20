16f0690f0d090bd8e7aebcbda3050e20f76784a4
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-20 10:55:02 +0300

    Added message hub in order to send and receive messages

--- HelloWorldWeb/HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/HelloWorldWeb/Startup.cs
@@ -56,6 +56,8 @@ namespace HelloWorldWeb
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -91,6 +93,7 @@ namespace HelloWorldWeb
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
+                endpoints.MapHub<MessageHub>("/messagehub");
             });
         }
     }
