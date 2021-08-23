bb181647a15b7c54958a02e1b17703821849653c
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-08-23 23:05:57 +0300

    added broadcast services

---
+++ HelloWorldWeb/HelloWorldWeb/Services/BroadcastService.cs
@@ -0,0 +1,34 @@
+﻿using HelloWorldWeb;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldWebApp.Services
+{
+    public class BroadcastService : IBroadcastService
+    {
+        private readonly IHubContext<MessageHub> messageHub;
+
+        public BroadcastService(IHubContext<MessageHub> messageHub)
+        {
+            this.messageHub = messageHub;
+        }
+
+        public void NewTeamMemberAdded(string name, int id)
+        {
+            messageHub.Clients.All.SendAsync("NewTeamMemberAdded", name, id);
+        }
+
+        public void TeamMemberDeleted(int id)
+        {
+            messageHub.Clients.All.SendAsync("TeamMemberDeleted", id);
+        }
+
+        public void UpdateTeamMember(string name, int id)
+        {
+            messageHub.Clients.All.SendAsync("UpdateTeamMember", name, id);
+        }
+    }
+}
---
+++ HelloWorldWeb/HelloWorldWeb/Services/IBroadcastService.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloWorldWebApp.Services
+{
+    public interface IBroadcastService
+    {
+        void NewTeamMemberAdded(string name, int id);
+        void TeamMemberDeleted(int id);
+        void UpdateTeamMember(string name, int id);
+    }
+}
